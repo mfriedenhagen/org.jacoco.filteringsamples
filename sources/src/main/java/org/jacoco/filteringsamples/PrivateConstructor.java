b58e548ddae745ff8f4293331875b68cb2ada97e
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2014-01-09 22:37:45 +0100

    Init

---
+++ sources/src/main/java/org/jacoco/filteringsamples/PrivateConstructor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jacoco.filteringsamples;
+
+/**
+ *
+ * @author Mirko Friedenhagen
+ */
+public class PrivateConstructor {
+
+    private PrivateConstructor() {
+
+    }
+}
